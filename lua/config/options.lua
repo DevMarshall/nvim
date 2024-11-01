67e6e13ec37eca86348ecef0036f015ff378c2fc
Gabriel Mania <gabrielmania11@proton.me>
2024-11-01 10:42:37 +0800

    No clipboard setting when on WSL and tmux

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,13 +2,18 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+local is_tmux = os.getenv("TMUX")
+
 -- Some OS detectors
 local is_wsl = vim.fn.has("wsl") == 1
 -- local is_mac = vim.fn.has("macunix") == 1
 -- local is_linux = not is_wsl and not is_mac
 
--- WSL Clipboard support
-if is_wsl then
+if is_wsl and is_tmux then
+  -- Do nothing
+  -- Don't set any clipboard setting when using tmux in WSL
+elseif is_wsl then
+  -- WSL Clipboard support
   -- This is NeoVim's recommended way to solve clipboard sharing if you use WSL
   -- See: https://github.com/neovim/neovim/wiki/FAQ#how-to-use-the-windows-clipboard-from-wsl
   vim.g.clipboard = {
